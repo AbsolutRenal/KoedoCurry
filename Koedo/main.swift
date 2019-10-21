c8788e8ffd59b1420034a529c6e23dedbeeac97c
Renaud Cousin <absolut.renal@hotmail.fr>
2019-10-21 16:36:04 +0200

    Initial Commit

---
+++ Koedo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Koedo
+//
+//  Created by Renaud Cousin on 10/21/19.
+//  Copyright © 2019 AbsolutRenal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
